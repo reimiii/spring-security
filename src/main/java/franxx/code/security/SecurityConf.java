0802d9958bd078cb0183f6d96d9d8b269b37078b
Hilmi AM <imiia75775@proton.me>
2024-07-13 23:04:36 +0700

    role based auth

---
+++ src/main/java/franxx/code/security/SecurityConf.java
@@ -0,0 +1,23 @@
+package franxx.code.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConf {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity.authorizeHttpRequests(registry -> {
+      registry.requestMatchers("/home").permitAll();
+      registry.requestMatchers("/admin/**").hasRole("ADMIN");
+      registry.requestMatchers("/user/**").hasRole("USER");
+      registry.anyRequest().authenticated();
+    }).build();
+  }
+
+}
