31a59f78f7d69a3d2951e9eb2436ab58d1547b5a
Hilmi AM <imiia75775@proton.me>
2024-07-13 23:22:25 +0700

    in memory user auth

--- src/main/java/franxx/code/security/SecurityConf.java
+++ src/main/java/franxx/code/security/SecurityConf.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +24,28 @@ public class SecurityConf {
       registry.requestMatchers("/admin/**").hasRole("ADMIN");
       registry.requestMatchers("/user/**").hasRole("USER");
       registry.anyRequest().authenticated();
-    }).build();
+    }).formLogin(AbstractAuthenticationFilterConfigurer::permitAll).build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails normalUser = User.builder()
+        .username("me")
+        .password("$2a$12$wm8tQFFbYMSRaQtiB/io5.iaVMGlRgmMN7RpJhYcdk6/.dH6nIWee")
+        .roles("USER")
+        .build();
+    UserDetails adminUser = User.builder()
+        .username("admin")
+        .password("$2a$12$wm8tQFFbYMSRaQtiB/io5.iaVMGlRgmMN7RpJhYcdk6/.dH6nIWee")
+        .roles("USER", "ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(adminUser, normalUser);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
